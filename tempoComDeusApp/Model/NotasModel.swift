e2728002ffb7bd0ea396bf4068a2d03c32726c9b
lidiane <lidianegomesbarbosa@gmail.com>
2020-08-13 19:40:44 -0300

    assets e notas model

---
+++ tempoComDeusApp/Model/NotasModel.swift
@@ -0,0 +1,9 @@
+//
+//  NotasModel.swift
+//  tempoComDeusApp
+//
+//  Created by Lidiane Gomes Barbosa on 13/08/20.
+//  Copyright © 2020 Lidiane Gomes Barbosa. All rights reserved.
+//
+
+import Foundation
---
+++ tempoComDeusApp/View/NotasCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  NotasCollectionViewCell.swift
+//  tempoComDeusApp
+//
+//  Created by Lidiane Gomes Barbosa on 13/08/20.
+//  Copyright © 2020 Lidiane Gomes Barbosa. All rights reserved.
+//
+
+import UIKit
+
+class NotasCollectionViewCell: UICollectionViewCell {
+    
+}
