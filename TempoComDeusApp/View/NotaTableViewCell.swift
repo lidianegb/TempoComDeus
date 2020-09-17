52ded4488492c482bcc07a3f755087501553ef86
lidiane <lidianegomesbarbosa@gmail.com>
2020-09-17 18:11:49 -0300

    estrutura para adicionar versiculo a nota

---
+++ TempoComDeusApp/View/NotaTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NotaTableViewCell.swift
+//  TempoComDeusApp
+//
+//  Created by Lidiane Gomes Barbosa on 17/09/20.
+//  Copyright © 2020 Lidiane Gomes Barbosa. All rights reserved.
+//
+
+import UIKit
+
+class NotaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TempoComDeusApp/View/VersiculoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VersiculoTableViewCell.swift
+//  TempoComDeusApp
+//
+//  Created by Lidiane Gomes Barbosa on 17/09/20.
+//  Copyright © 2020 Lidiane Gomes Barbosa. All rights reserved.
+//
+
+import UIKit
+
+class VersiculoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
