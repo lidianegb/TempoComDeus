8de73e104d6820d962f53d4a9d3747d43c79032a
Lidiane <lidianegomesbarbosa@gmail.com>
2020-10-06 15:24:26 -0300

    removida backView

--- TempoComDeusApp/View/NotaTableViewCell.swift
+++ TempoComDeusApp/View/NotaTableViewCell.swift
@@ -9,14 +9,14 @@
 import UIKit
 
 class NotaTableViewCell: UITableViewCell, UITextViewDelegate {
-    let backView = BackView()
+   
     var nota: Nota = Nota(body: nil, cor: nil, versos: [])
     
     var color: String? {
         didSet {
             nota.cor = color ?? ""
             delegate?.getNota(nota: nota)
-            backView.backgroundColor = .getColor(name: color ?? "nota1")
+            textView.backgroundColor = .getColor(name: color ?? "nota1")
         }
     }
     weak var delegate: NewNotaDelegate?
@@ -25,9 +25,11 @@ class NotaTableViewCell: UITableViewCell, UITextViewDelegate {
         let text = UITextView()
         text.isScrollEnabled = false
         text.textAlignment = .left
-        text.backgroundColor = .clear
+        text.textContainerInset = UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 16)
         text.font = UIFont.systemFont(ofSize: 20)
         text.textColor = .label
+        text.layer.cornerRadius = 8
+        text.clipsToBounds = true
         return text
     }()
     
@@ -36,7 +38,7 @@ class NotaTableViewCell: UITableViewCell, UITextViewDelegate {
     }
     func createCell() {
         contentView.backgroundColor = .backgroundColor
-        addBackView()
+    
         addTextView()
     }
     func textViewDidChange(_ textView: UITextView) {
@@ -47,19 +49,20 @@ class NotaTableViewCell: UITableViewCell, UITextViewDelegate {
     }
 
     func addTextView() {
+        textView.backgroundColor = .getColor(name: color ?? "")
         textView.addDoneButton(title: "Done", target: self, selector: #selector(tapDone(sender:)))
         textView.text = "O que está no seu coração?"
         textView.textColor = .secondaryLabel
         textView.delegate = self
-        backView.addSubview(textView)
-        textView.anchor(top: backView.topAnchor,
-                        left: backView.leftAnchor,
-                        bottom: backView.bottomAnchor,
-                        right: backView.rightAnchor,
-                        paddingTop: 0,
-                        paddingLeft: 16,
-                        paddingBottom: 0,
-                        paddingRight: 16)
+        contentView.addSubview(textView)
+        textView.anchor(top: contentView.topAnchor,
+                        left: contentView.leftAnchor,
+                        bottom: contentView.bottomAnchor,
+                        right: contentView.rightAnchor,
+                        paddingTop: 16,
+                        paddingLeft: 0,
+                        paddingBottom: 16,
+                        paddingRight: 0)
     }
     
     func textViewDidBeginEditing(_ textView: UITextView) {
@@ -69,32 +72,15 @@ class NotaTableViewCell: UITableViewCell, UITextViewDelegate {
         }
     }
     
+    func textViewDidChangeSelection(_ textView: UITextView) {
+        delegate?.updateHeightOfRow(self, textView)
+    }
+    
     func textViewDidEndEditing(_ textView: UITextView) {
         if textView.text.isEmpty {
             textView.text = "O que está no seu coração?"
             textView.textColor = .secondaryLabel
         }
-    }
- 
-    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-        let currentText = textView.text ?? ""
-        guard let stringRange = Range(range, in: currentText) else { return false }
-        let updatedText = currentText.replacingCharacters(in: stringRange, with: text)
-        return updatedText.count <= 25000
-    }
-    
-    private func addBackView() {
-        backView.layer.cornerRadius = 8
-        backView.backgroundColor = .getColor(name: color ?? "")
-        contentView.insertSubview(backView, at: 0)
-        backView.anchor(top: contentView.topAnchor,
-                        left: contentView.leftAnchor,
-                        bottom: contentView.bottomAnchor,
-                        right: contentView.rightAnchor,
-                        paddingTop: 0,
-                        paddingLeft: 0,
-                        paddingBottom: 0,
-                        paddingRight: 0)
     }
  
     override func setSelected(_ selected: Bool, animated: Bool) {
