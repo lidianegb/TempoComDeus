b7bfb63884033ee8b0e52c8d76495f81e9389cf0
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-01-06 11:32:29 -0300

    refatorando

---
+++ TempoComDeusApp/Controller/LivrosTableViewDelegates.swift
@@ -0,0 +1,9 @@
+//
+//  LivrosTableViewDelegates.swift
+//  TempoComDeusApp
+//
+//  Created by Lidiane Gomes Barbosa on 06/01/21.
+//  Copyright © 2021 Lidiane Gomes Barbosa. All rights reserved.
+//
+
+import Foundation
