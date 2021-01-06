ea6fb627f118f218cf6bb51fa8b17c2019ae655a
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-01-06 11:34:16 -0300

    refatorando collection view delegates

--- TempoComDeusApp/Controller/NotasCollectionDelegates.swift
+++ TempoComDeusApp/Controller/NotasCollectionDelegates.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+import UIKit
+
+extension NotasViewController: UICollectionViewDataSource, UICollectionViewDelegate,
+UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+         notas.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId,
+                                                              for: indexPath) as? NotasCollectionViewCell
+            else {return UICollectionViewCell()}
+         
+        myCell.nota = notas[indexPath.row]
+        myCell.createCell()
+        myCell.delegate = self
+        return myCell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let collectionWidth = collectionView.frame.width
+        let collectionHeight = collectionView.frame.height
+        return calcCellWidthAndHeight(width: collectionWidth, height: collectionHeight)
+    }
+    
+    func calcCellWidthAndHeight(width: CGFloat, height: CGFloat) -> CGSize {
+        let cellWidth =  width / 2 - 15
+        let cellHeigth = height / 5 - 10
+        
+        return CGSize(width: cellWidth, height: cellHeigth)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    
+        let visualizarNota = VisualizarNotaViewController(notaRepository: notaRepository,
+                                                          notaId: notas[indexPath.row].notaId)
+        visualizarNota.delegate = self
+        visualizarNota.modalPresentationStyle = .fullScreen
+        self.navigationController?.pushViewController(visualizarNota, animated: true)
+    }
+
+}
--- TempoComDeusApp/Controller/NotasViewController.swift
+++ TempoComDeusApp/Controller/NotasViewController.swift
@@ -35,7 +35,7 @@ class NotasViewController: UIViewController {
         return collectionView
     }()
     
-    private var notaRepository: NotaRepository {
+    var notaRepository: NotaRepository {
           NotaRepository()
     }
     
@@ -147,49 +147,6 @@ class NotasViewController: UIViewController {
        }
 }
 
-extension NotasViewController: UICollectionViewDataSource, UICollectionViewDelegate,
-UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        notas.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView,
-                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId,
-                                                              for: indexPath) as? NotasCollectionViewCell
-            else {return UICollectionViewCell()}
-        
-        myCell.nota = notas[indexPath.row]
-        myCell.createCell()
-        myCell.delegate = self
-        return myCell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                        sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let collectionWidth = collectionView.frame.width
-        let collectionHeight = collectionView.frame.height
-        return calcCellWidthAndHeight(width: collectionWidth, height: collectionHeight)
-    }
-    
-    func calcCellWidthAndHeight(width: CGFloat, height: CGFloat) -> CGSize {
-        let cellWidth =  width / 2 - 15
-        let cellHeigth = height / 5 - 10
-        
-        return CGSize(width: cellWidth, height: cellHeigth)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
-        let visualizarNota = VisualizarNotaViewController(notaRepository: notaRepository,
-                                                          notaId: notas[indexPath.row].notaId)
-        visualizarNota.delegate = self
-        visualizarNota.modalPresentationStyle = .fullScreen
-        self.navigationController?.pushViewController(visualizarNota, animated: true)
-    }
-
-}
 extension NotasViewController: NotasCellDelegate {
     func deleteCell(cell: NotasCollectionViewCell) {
         if let indexPath = collectionView.indexPath(for: cell) {
