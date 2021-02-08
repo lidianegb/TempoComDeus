48c092b5671f07bbd419dcc2e83886cdb66bb4a2
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-02-08 11:07:37 -0300

    context menus

---
+++ TempoComDeusApp/Controller/Bible/ContextMenuExtension.swift
@@ -0,0 +1,9 @@
+//
+//  ContextMenuExtension.swift
+//  TempoComDeusApp
+//
+//  Created by Lidiane Gomes Barbosa on 08/02/21.
+//  Copyright © 2021 Lidiane Gomes Barbosa. All rights reserved.
+//
+
+import Foundation
