876fededfdddb7fc54ff25ccbed805e4d8bc3080
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-02-09 08:45:26 -0300

    chapter model

--- TempoComDeusApp/Model/Chapter.swift
+++ TempoComDeusApp/Model/Chapter.swift
@@ -55,7 +55,7 @@ class Chapter: Codable {
     }
     
     func getSelectedVerses(selectedIndexes: [Int]) -> String {
-        var formattedText: String = bookName + " \(number)"
+        var formattedText: String = bookName + " \(number + 1)"
         for index in selectedIndexes {
             formattedText += "\n\(index + 1). " + versicles[index]
         }
