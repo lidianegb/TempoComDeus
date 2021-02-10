55cdc8ff3b1c45fcb548eedb8383038d9eddb201
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-02-10 10:07:56 -0300

    teste notifications

--- TempoComDeusApp/Controller/Bible/NotificationController.swift
+++ TempoComDeusApp/Controller/Bible/NotificationController.swift
@@ -7,3 +7,65 @@
 //
 
 import Foundation
+import UserNotifications
+class NotificationsController {
+    let bible = Bible()
+    let title: String
+    let subtitle: String
+    let body: String
+    let center: UNUserNotificationCenter
+    
+    init(center: UNUserNotificationCenter) {
+        self.center = center
+        (subtitle, body) = Bible().generateRamdomVerse()
+        title = "Você um tempo para Deus?"
+    }
+    
+    func schenduleNotificationNonRepeat() {
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
+        content.subtitle = subtitle
+        content.sound = UNNotificationSound.default
+
+        let trigger = UNTimeIntervalNotificationTrigger(
+          timeInterval: 10,
+          repeats: false)
+        let request = UNNotificationRequest(
+            identifier: UUID().uuidString,
+          content: content,
+          trigger: trigger)
+        center.add(request)
+        
+    }
+    
+    func schenduleNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.subtitle = subtitle
+        content.body = body
+        content.sound = .default
+            
+        let date = Date().addingTimeInterval(10)
+    //    let date = Date().addingTimeInterval(24 * 60 * 60) // -> 24 horas
+//        let dateComponents = Calendar.current.dateComponents(
+//            [.year, .month, .day, .hour, .minute, .second], from: date)
+        let dateComponents = Calendar.current.dateComponents(
+                [.hour, .minute, .second], from: date)
+
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+
+        // -------- Create the request ------------
+        let uuidString = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+
+        // -------- Register the request -----------
+        center.add(request) { (error) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+}
--- TempoComDeusApp/Shared/AppDelegate.swift
+++ TempoComDeusApp/Shared/AppDelegate.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 import CoreData
+import UserNotifications
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions
         launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) {_, error in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+        
+       let notification = NotificationsController(center: center)
+        notification.schenduleNotification()
         return true
     }
 
