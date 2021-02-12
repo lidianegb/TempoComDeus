440b711b431fa3e8b5a3f880d500c124947ddefe
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-02-12 10:47:37 -0300

    testes biblia model

--- ApplicationTests/TestBible.swift
+++ ApplicationTests/TestBible.swift
@@ -4,30 +4,61 @@
 //
 //  Created by Lidiane Gomes Barbosa on 12/02/21.
 //  Copyright © 2021 Lidiane Gomes Barbosa. All rights reserved.
-//
+// swiftlint:disable all
 
 import XCTest
-
+@testable import tempo_com_Deus
 class TestBible: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var sut: Bible!
+    override func setUp() {
+        sut = Bible(version: "nvi", abbreviation: "gn")
+        super.setUp()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testUpdateActualBook() {
+        let actualAbrreviation = sut.abbreviation
+        let newAbbreviation = "lc"
+        sut.updateActualBook(abbreviation: newAbbreviation)
+        XCTAssertNotEqual(sut.abbreviation, actualAbrreviation)
+        XCTAssertEqual(sut.abbreviation, newAbbreviation)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testUpdateActualVersion() {
+        let actualVersion = sut.version
+        let newVersion = "aa"
+        sut.updateActualVersion(version: newVersion)
+        XCTAssertEqual(sut.version, newVersion)
+        XCTAssertNotEqual(sut.version, actualVersion)
     }
-
+    
+    func testGetNextBook() {
+        let nextBook = sut.getNextBook()
+        XCTAssertNotNil(nextBook)
+        let nextAbbreviation = "ex"
+        let abbrev = nextBook!.abbreviation["pt"]
+        XCTAssertEqual(abbrev, nextAbbreviation)
+    }
+    
+    func testGetNextBook_returnNil() {
+        sut.updateActualBook(abbreviation: "ap")
+        XCTAssertNil(sut.getNextBook())
+    }
+    
+    func testGetPreviewBook_returnNil() {
+        XCTAssertNil(sut.getPreviewBook())
+    }
+    
+    func testGetPreviewBook() throws {
+        sut.updateActualBook(abbreviation: "lc")
+        let previewAbbreviation = "mc"
+        let previewBook = sut.getPreviewBook()
+        XCTAssertNotNil(previewBook)
+        let abbrev = previewBook!.abbreviation["pt"]
+        XCTAssertEqual(abbrev, previewAbbreviation)
+    }
 }
