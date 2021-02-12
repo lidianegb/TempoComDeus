d2c52bac60bd351d813872016da61970fc278e95
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-02-12 12:04:30 -0300

    ui testes

---
+++ TempoComDeusUITests/TestConfigTableView.swift
@@ -0,0 +1,9 @@
+//
+//  TestConfigTableView.swift
+//  ApplicationTests
+//
+//  Created by Lidiane Gomes Barbosa on 12/02/21.
+//  Copyright © 2021 Lidiane Gomes Barbosa. All rights reserved.
+//
+
+import Foundation
