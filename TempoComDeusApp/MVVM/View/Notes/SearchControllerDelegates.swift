a7243765516d96657a6ee51d485c28be262b6cac
Lidiane Gomes <lidianegomesbarbosa@gmail.com>
2021-02-16 14:34:15 -0300

    seach controller

---
+++ TempoComDeusApp/MVVM/View/Notes/SearchControllerDelegates.swift
@@ -0,0 +1,9 @@
+//
+//  SearchControllerDelegates.swift
+//  TempoComDeusApp
+//
+//  Created by Lidiane Gomes Barbosa on 16/02/21.
+//  Copyright © 2021 Lidiane Gomes Barbosa. All rights reserved.
+//
+
+import Foundation
